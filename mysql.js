fcec1951a873fad111222bfab19112a24a6069ce
Ga-yo <39812@naver.com>
2021-03-22 20:11:24 +0900

    [Remove] mysql.js

--- mysql.js
+++
@@ -1,9 +0,0 @@
-var mysql = require('mysql');
-var connect = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : 'password',
-    databsae : 'example'
-});
-
-connect.connect();
