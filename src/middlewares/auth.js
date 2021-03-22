3685a85d9bf9bf99cf50e1a725beb35074aab72e
Ga-yo <39812@naver.com>
2021-03-22 20:13:05 +0900

    [Add] authMiddleware jwt check

---
+++ src/middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = async (req, res, next) => {
+    const token = req.headers["access-token"];
+    
+    // token does not exist
+    if (!token) {
+        res.status(403).json({
+            success: false,
+            message: "token required"
+        });
+    }
+
+    // create a promise that decodes the token
+    try {
+        await jwt.verify(token, req.app.get("jwt-secret"), (err, decode) => {
+            if (err) rejects(err)
+            req.decode = decode;
+            next();
+        });
+    }catch {
+        res.status(401).json({ message: e.message});
+    }
+};
+
+module.exports = authMiddleware
