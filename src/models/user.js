965817840b0d678dcdeaf9c9009344642157ef26
Ga-yo <39812@naver.com>
2021-03-23 18:59:49 +0900

    [Add] user model

---
+++ src/models/user.js
@@ -0,0 +1,18 @@
+
+module.exports = (sequelize, DataTypes) => {
+    return sequelize.define("user", {
+        userId: {
+            type: DataTypes.STRING(100),
+            unique: true,
+            primaryKey: true,
+        },
+        password: {
+            type: DataTypes.STRING(100),
+            allowNull: false,
+        },
+        name: {
+            type: DataTypes.STRING(20),
+            allowNull: false,
+        },
+    });
+};
