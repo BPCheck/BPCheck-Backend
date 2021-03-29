174014a63bf3062822ef7ee057e2ca60b40fd1a7
Ga-yo <39812@naver.com>
2021-03-29 23:05:41 +0900

    [Fix] authMiddleware token test

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,26 +1,31 @@
 const jwt = require('jsonwebtoken');
 
 const authMiddleware = async (req, res, next) => {
-    const token = req.headers["access-token"];
-    
+    let token;
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith('Bearer')
+      ) {
+        token = req.headers.authorization.split(' ')[1];
+      } 
+
     // token does not exist
     if (!token) {
-        res.status(403).json({
-            success: false,
-            message: "token required"
-        });
+        res.status(403).json({ message: "token required" });
+        return;
     }
 
     // create a promise that decodes the token
     try {
         await jwt.verify(token, req.app.get("jwt-secret"), (err, decode) => {
-            if (err) rejects(err)
+            if (err) throw new Error(err);
             req.decode = decode;
             next();
         });
-    }catch {
+    }catch (e) {
+        console.log(token.authorization);
         res.status(401).json({ message: e.message});
     }
 };
 
-module.exports = authMiddleware
+module.exports = authMiddleware;
