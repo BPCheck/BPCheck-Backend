72c9f42661a0d0ef42d7c23e25f2512d6f1d2239
Ga-yo <39812@naver.com>
2021-03-29 23:06:23 +0900

    [Add] pulse

--- src/controller/bp/controller.js
+++ src/controller/bp/controller.js
@@ -1,15 +1,16 @@
 const { Bp } = require('../../models');
 
 const createBp = async (req, res, next) => {
-    const { highBp, lowBp, date } = req.body;
-    const userId = req.decoded.userId;
+    const { highBp, lowBp, pulse,date } = req.body;
+    const userId = req.decode.userId;
 
     try {
         const bp = await Bp.findOne({ where: {userId, date} });
         if (bp) throw new Error('이미 해당 날짜에 작성한 이력이 있음');
-        await Bp.create({ userId, highBp, lowBp, date });
+        await Bp.create({ userId, highBp, lowBp, pulse, date });
         res.status(200).end();
     } catch (e) {
+        console.log(e.message);
         res.status(409).end();
     }
 };
